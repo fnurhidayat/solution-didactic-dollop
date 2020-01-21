492ed5f9fb698f7e39a54d62d211bf247ad8574c
FikriRNurhidayat <fnurhidayat@binar.co.id>
2020-01-21 10:17:20 +0700

    [Fikk] add switch for operation

--- index.js
+++ index.js
@@ -12,12 +12,13 @@
 var create = require('./create.js');
 var args = process.argv.slice(2);
 
-if (args.length < 2) {
-  process.exit();
+switch(args[0]) {
+  case 'create_users':
+    var email = args[0];
+    var password = args[1];
+    var password_confirmation = args[2];
+
+    create(email, password, password_confirmation);
+  default:
+    console.log('Unknown operation!')
 }
-
-var email = args[0];
-var password = args[1];
-var password_confirmation = args[2];
-
-create(email, password, password_confirmation);
