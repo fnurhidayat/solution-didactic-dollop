8854a7bda495bd5060b206b1fa69e7760e9f99fa
FikriRNurhidayat <fnurhidayat@binar.co.id>
2020-01-23 09:34:07 +0700

    [Fikk] Active Record

--- models/index.js
+++ models/index.js
@@ -8,7 +8,26 @@ class ActiveRecord {
   }
  
   save() {
-    fs.writeFileSync(`./${this.table_name}.json`, JSON.stringify(this.data, null, 2)); 
+    let files;
+    fs.readdir(`${__dirname}/../data/`, (err, data) => {
+      files = data;
+
+      let filename = this.table_name + '.json';
+
+      files = files.filter(i => i == filename);
+      if (files.length == 0) {
+        fs.writeFileSync(`${__dirname}/../data/${filename}`, '[]');
+      }
+
+      let dataFile = require(`${__dirname}/../data/${filename}`);
+      dataFile.push(this.data);
+
+      fs.writeFileSync(`${__dirname}/../data/${filename}`, JSON.stringify(this.data, null, 2)); 
+    })
+  }
+
+  static find(id) {
+    console.log(id)
   }
 }
 
